7a25f8bdd1165d3400edf6d3a7fc82599d5833e6
Siwat1511 <beamsiwat1511@gmail.com>
2025-06-24 16:58:24 +0000

    6/24

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+fnumb = int(input("Enter the first number: "))
+print (int(fnumb))
+snumb = int(input("Enter the second number: "))
+print (int(snumb))
+print (int(fnumb)),("x"),((snumb))
+result = (int(fnumb)*int(snumb))
+print (int(result))
+if result < 0 :
+    print("The result is negative.")
+elif result > 0 :
+    print("The result is positive.")
+else :
+  print("The result is positive and negative.")
